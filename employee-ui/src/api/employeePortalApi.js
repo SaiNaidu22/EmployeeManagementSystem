d00ae753dc4b188aaee40d220978afb4e590b7bf
SaiNaidu22 <sain41159@gmail.com>
2026-06-27 17:32:03 +0530

    Use environment variable for API URL

--- employee-ui/src/api/employeePortalApi.js
+++ employee-ui/src/api/employeePortalApi.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-const BASE_URL =
-  "http://localhost:5157/api/Employee";
+const API_URL =
+  import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL: API_URL
+});
+
+
 
 const getAuthHeader = () => ({
   headers: {
